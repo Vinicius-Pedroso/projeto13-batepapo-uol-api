8d9d74ecc546fd3cd644acf2d89cadb9e47eacd1
Vinicius <viniciuspedrosoreis@gmail.com>
2022-11-14 12:21:13 -0300

    mongo corrections

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -9,139 +9,126 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-let participationValid = true;
-
 const mongoClient = new MongoClient(process.env.MONGO_URI);
 let db;
-mongoClient.connect().then(() => {
-	db = mongoClient.db("batePapoUol");
-    users = db.collection("users");
-}).catch(error => console.log(error));
-
-const joi = require('joi');
+try {
+    await mongoClient.connect()
+    db = mongoClient.db("batePapoUol");
+} catch (error){
+    console.log(error)
+}
 
 const participantSchema = joi.object({
     name: joi.string().required()
 });
 
-const messageSchema = joi.object({
-    to: joi.string().required(),
-    text: joi.string.required(),
-    type: joi.string().valid('message','private_message').required()
-});
-
-const dayjs = require('dayjs')
-dayjs().format()
+// const messageSchema = joi.object({
+//     to: joi.string().required(),
+//     text: joi.string().required(),
+//     type: joi.string().valid('message','private_message').required()
+// });
 
 app.post("/participants", async (req, res) => {
+    
+    try {
+
     const { name } = req.body;
-  
-    const validation = participantSchema.validate(name);
+    const validation = participantSchema.validate({name});
+    console.log(validation.error)
 
     if (validation.error) {
-        const erros = validation.error.details.map((detail) => detail.message);
-        res.status(422).send(erros);
-        return;
+        
+        return res.sendStatus(422);
     }
 
-    db.collection("users").findOne({
+    const verifyUser = await db.collection("users").findOne({
         name: name
-    }).then(user => {
-        participationValid = false
-        console.log(user);
+    })
+
+    if (verifyUser) {
         res.sendStatus(409);
-    });
-
-    if (participationValid === true) {
-        const user = {
-            name: name, 
-            lastStatus: Date.now()
-        }
-
-        try {
-            await db.collection("users").insertOne(user)
-            res.sendStatus(201);
-          } catch (error) {
-            console.error(error);
-            res.sendStatus(500);
-        }
-
-        const message = {
-            from: name, 
-            to: 'todos', 
-            text: 'entra na sala...', 
-            type: 'status',
-            time: 'HH:MM:SS'
-        }
-
-        try {
-            await db.collection("message").insertOne(message)
-            res.sendStatus(201);
-          } catch (error) {
-            console.error(error);
-            res.sendStatus(500);
-        }
     }
 
-    res.status(201).send();
-});
-
-app.get("/participants", (req, res) => {
-    
-    db.collection("users").find().toArray().then(users => {
-        res.send(users);
-    }).catch(error => {
-        res.sendStatus(500)
-        console.log(error)
-    });
-    
-});
-
-app.post("/messages", (req, res) => {
-    const { to, text, type } = req.body;
-    const from = req.header.user
-
-    db.collection("users").findOne({
-        name: from
-    }).then(user => {
-
-    });
+    const user = {
+        name: name,
+        lastStatus: Date.now()
+    }
 
     const message = {
-        to: to,
-        text: text,
-        type: type
-    }
-
-    const messageValidation = messageSchema.validate(message, {abortEarly: false});
-
-    if (messageValidation.error) {
-    console.log(validation.error.details)
-    }
-
-    const messageDone = {
-        to: to,
-        text: text,
-        type: type,
-        from: from,
+        from: name,
+        to: 'todos',
+        text: 'entra na sala...',
+        type: 'status',
         time: 'HH:MM:SS'
     }
 
-    res.status(201).send();
-})
+    
+        await db.collection("users").insertOne(user)
+        await db.collection("message").insertOne(message)
+        res.sendStatus(201);
+    } catch (error) {
+        console.error(error);
+        res.sendStatus(500);
+    }
 
-app.post("/status", (req, res) => {
-    const user = req.header.user
+});
 
-    db.collection("users").findOne({
-        name: user
-    }).then(user => {
-        console.log(user);
-    }).catch(error => {
-        res.sendStatus(404)
-    });
+// app.get("/participants", (req, res) => {
+    
+//     db.collection("users").find().toArray().then(users => {
+//         res.send(users);
+//     }).catch(error => {
+//         res.sendStatus(500)
+//         console.log(error)
+//     });
+    
+// });
 
-    res.status(200).send();
-})
+// app.post("/messages", (req, res) => {
+//     const { to, text, type } = req.body;
+//     const from = req.header.user
+
+//     db.collection("users").findOne({
+//         name: from
+//     }).then(user => {
+
+//     });
+
+//     const message = {
+//         to: to,
+//         text: text,
+//         type: type
+//     }
+
+//     const messageValidation = messageSchema.validate(message, {abortEarly: false});
+
+//     if (messageValidation.error) {
+//     console.log(validation.error.details)
+//     }
+
+//     const messageDone = {
+//         to: to,
+//         text: text,
+//         type: type,
+//         from: from,
+//         time: 'HH:MM:SS'
+//     }
+
+//     res.status(201).send();
+// })
+
+// app.post("/status", (req, res) => {
+//     const user = req.header.user
+
+//     db.collection("users").findOne({
+//         name: user
+//     }).then(user => {
+//         console.log(user);
+//     }).catch(error => {
+//         res.sendStatus(404)
+//     });
+
+//     res.status(200).send();
+// })
 
 app.listen(5000);
