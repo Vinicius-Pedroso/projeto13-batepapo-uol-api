83956211b2e9ecde309da71ff2f8045aa192bec5
Vinicius <viniciuspedrosoreis@gmail.com>
2022-11-14 00:11:12 -0300

    minor errors and organization

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -2,31 +2,33 @@ import express from 'express'
 import cors from 'cors'
 import joi from 'joi'
 import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
-const joi = require('joi');
+app.use(cors());
+app.use(express.json());
 
-let participationValid = true
+let participationValid = true;
 
 const mongoClient = new MongoClient("mongodb://localhost:27017");
 let db;
 mongoClient.connect().then(() => {
-	db = mongoClient.db("BatePapoUol");
-});
+	db = mongoClient.db("batePapoUol");
+    users = db.collection("users");
+}).catch(error => console.log(error));
+
+const joi = require('joi');
 
 const participantSchema = joi.object({
     name: joi.string().required()
-})
+});
 
 const messageSchema = joi.object({
     to: joi.string().required(),
     text: joi.string.required(),
     type: joi.string().valid('message','private_message').required()
-})
-
-
-app.use(cors());
-app.use(express.json())
+});
 
 app.post("/participants", async (req, res) => {
     const { name } = req.body;
@@ -82,20 +84,44 @@ app.post("/participants", async (req, res) => {
 });
 
 app.get("/participants", (req, res) => {
+    
     db.collection("users").find().toArray().then(users => {
-        console.log(users);
+        res.send(users);
+    }).catch(error => {
+        res.sendStatus(500)
+        console.log(error)
     });
-
-})
+    
+});
 
 app.post("/messages", (req, res) => {
     const { to, text, type } = req.body;
     const from = req.header.user
 
+    db.collection("users").findOne({
+        name: from
+    }).then(user => {
+
+    });
+
+    const message = {
+        to: to,
+        text: text,
+        type: type
+    }
+
     const messageValidation = messageSchema.validate(message, {abortEarly: false});
 
     if (messageValidation.error) {
     console.log(validation.error.details)
+    }
+
+    const messageDone = {
+        to: to,
+        text: text,
+        type: type,
+        from: from,
+        /*time: dayjsfunction*/
     }
 
     res.status(201).send();
